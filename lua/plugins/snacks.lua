dffc53377e5af6e12f24c35c639592a514654fe1
hayden <marziply@gmail.com>
2025-03-16 15:30:48 +0000

    feat: replace plugins with snacks.nvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,48 @@
+return {
+	{
+		"folke/snacks.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			animate = {
+				enabled = true,
+			},
+			bufdelete = {
+				enabled = false,
+			},
+			dashboard = {
+				enabled = true,
+			},
+			debug = {
+				enabled = true,
+			},
+			git = {
+				enabled = true,
+			},
+			indent = {
+				enabled = true,
+			},
+			notifier = {
+				enabled = true,
+			},
+			picker = {
+				enabled = true,
+			},
+			scope = {
+				enabled = true,
+			},
+			statuscolumn = {
+				enabled = true,
+			},
+			terminal = {
+				enabled = true,
+			},
+			words = {
+				enabled = true,
+			},
+			win = {
+				enabled = true,
+			},
+		},
+	},
+}
